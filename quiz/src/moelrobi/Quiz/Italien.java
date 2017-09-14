2530883b04785ad1f44a45a7ad97870533160e78
RobStyling <moelrobi@gmail.com>
2017-09-14 16:08:45 +0200

    I made a New Frame Form to be easier to edit. No Hardcoding Allowed here! :D

--- quiz/src/moelrobi/Quiz/Italien.java
+++ quiz/src/moelrobi/Quiz/Italien.java
@@ -1,10 +1,4 @@
 package moelrobi.Quiz;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
 /**
  * @version v.1.0
  * @author robin
@@ -13,37 +7,21 @@ public class Italien {
     /*
         Declaring Variables
     */ 
-    private static JFrame frame;
-     private static JLabel label1;
-     
+    private static final String[] questions = {"Was ist die Hauptstadt von Rom?"};
     /**
-     * Method for init. the Components for JFrame
-     * @param title, needed for Displaying a Title
-     * @return a  JFrame
-     */    
-     public static JFrame initComp(String title) {
-            frame = new JFrame(title);
-            frame.setSize(200, 200);
-            frame.setVisible(true);
-            return frame;
-        } 
-     /**
-      * Loading Labels
-      */
-     public static void Labels() 
-     {
-            label1 = new javax.swing.JLabel("Hello");
-             JButton butt1 = new JButton("ok");
-             frame.add(butt1);
-     }
-    
-      /**
-       * Main class.
-        * @param args, for parsing Arguments
-        */
+    * Main class.
+    * @param args, for parsing Arguments
+    */
     public static void main(String[] args) {
         // TODO code application logic here
-        initComp("Testing");
-        Labels();
+        System.out.print(questions[0]);
+        Frame frame = new Frame();
+        frame.setVisible(true);
+    }
+    
+    public static String CheckAnswer(String Answer) 
+    {
+        System.out.print(Answer);
+        return null;
     }
 }
